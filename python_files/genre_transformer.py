c0ed5cae2482a23586b83dfe8ca5324b106339fb
pkirsch <philippe.kirsch@hotmail.fr>
2022-12-06 12:00:13 +0400

    black, isort and readme

--- python_files/genre_transformer.py
+++ python_files/genre_transformer.py
@@ -1,5 +1,6 @@
-from sklearn.base import TransformerMixin, BaseEstimator
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+
 
 class GenreTranformer(TransformerMixin, BaseEstimator):
     # BaseEstimator generates the get_params() and set_params() methods that all Pipelines require
@@ -11,10 +12,10 @@ class GenreTranformer(TransformerMixin, BaseEstimator):
     def fit(self, X, y=None):
         self.unique_genre_list = []
         for genres in X:
-            for genre in genres[0].split('|'):
+            for genre in genres[0].split("|"):
                 if genre not in self.unique_genre_list:
                     self.unique_genre_list.append(genre)
-#         print(len(self.unique_genre_list))
+        #         print(len(self.unique_genre_list))
         return self
 
     def transform(self, X, y=None):
@@ -22,9 +23,9 @@ class GenreTranformer(TransformerMixin, BaseEstimator):
         genre_transformed = []
         for genres in X:
             genre_np = np.zeros((len(unique_genre_list),), dtype=int)
-            for target_genre in genres[0].split('|'):
+            for target_genre in genres[0].split("|"):
                 if target_genre in unique_genre_list:
-                        index = unique_genre_list.index(target_genre)
-                        genre_np[index] = 1
+                    index = unique_genre_list.index(target_genre)
+                    genre_np[index] = 1
             genre_transformed.append(list(genre_np))
         return np.array(genre_transformed)
--- streamlit_test.py
+++ streamlit_test.py
@@ -1,21 +1,23 @@
-import streamlit as st
-import pandas as pd
-import imdb
-import shap
-import matplotlib.pyplot as plt
 import json
+
+import imdb
+import matplotlib.pyplot as plt
+import pandas as pd
 import requests
-st.set_option('deprecation.showPyplotGlobalUse', False)
+import shap
+import streamlit as st
+
+st.set_option("deprecation.showPyplotGlobalUse", False)
 import pickle
+
+import numpy as np
+
+from python_files.cast_transformer import CastTransformer
 from python_files.dataframe_transformer import DataframeTransformer
 from python_files.genre_transformer import GenreTranformer
-from python_files.cast_transformer import CastTransformer
-import numpy as np
 
 MODEL_PATH = "model/xgb_model.pkl"
 import base64
-
-
 
 tab1, tab2 = st.tabs(["Exitsing movies", "New movie"])
 
@@ -49,9 +51,10 @@ with tab1:
 
     # st.write(background_image_style(image_path), unsafe_allow_html=True)
 
-    original_title = '<p style="font-family:Courier; color:Black; font-size: 30px;">This app predicts the Movie Revenue!</p>'
+    original_title = (
+        '<p style="font-family:Courier; color:Black; font-size: 30px;">This app predicts the Movie Revenue!</p>'
+    )
     st.markdown(original_title, unsafe_allow_html=True)
-
 
     def user_input_features(movie_name):
         try:
@@ -63,82 +66,81 @@ with tab1:
             url = f'https://api.themoviedb.org/3/find/tt{imdb_id}?api_key={st.secrets["tmdb_key"]}&external_source=imdb_id'
             response = requests.get(url)
             info = response.json()
-            movie_id = info['movie_results'][0]['id']
+            movie_id = info["movie_results"][0]["id"]
 
             df = {}
             url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={st.secrets["tmdb_key"]}'
             response = requests.get(url)
 
-            backdrop_path = response.json()['backdrop_path']
+            backdrop_path = response.json()["backdrop_path"]
             st.image(
-                    "https://image.tmdb.org/t/p/original/" + backdrop_path,
-                    width=400, # Manually Adjust the width of the image as per requirement
-                )
-            df['budget'] = response.json()['budget']
-            df['release_date'] = response.json()['release_date']
-            df['production_companies'] = response.json()['production_companies'][0]['name']
-            df['production_companies_number'] = len(response.json()['production_companies'])
-            df['production_countries_number'] = len(response.json()['production_countries'])
-            df['runtime'] = response.json()['runtime']
-            df['production_countries'] = response.json()['production_countries'][0]['name']
-            genres = response.json()['genres']
+                "https://image.tmdb.org/t/p/original/" + backdrop_path,
+                width=400,  # Manually Adjust the width of the image as per requirement
+            )
+            df["budget"] = response.json()["budget"]
+            df["release_date"] = response.json()["release_date"]
+            df["production_companies"] = response.json()["production_companies"][0]["name"]
+            df["production_companies_number"] = len(response.json()["production_companies"])
+            df["production_countries_number"] = len(response.json()["production_countries"])
+            df["runtime"] = response.json()["runtime"]
+            df["production_countries"] = response.json()["production_countries"][0]["name"]
+            genres = response.json()["genres"]
             genre_list = []
             for i in range(len(genres)):
-                genre_list.append(genres[i]['name'])
+                genre_list.append(genres[i]["name"])
             genre_list = "|".join(genre_list)
-            df['genres'] = genre_list
-            df['popularity'] = response.json()['popularity']
-            df['vote_average'] = float(response.json()['vote_average'])
-            df['vote_count'] = int(response.json()['vote_count'])
-            df['belongs_to_collection'] = response.json()['belongs_to_collection']
+            df["genres"] = genre_list
+            df["popularity"] = response.json()["popularity"]
+            df["vote_average"] = float(response.json()["vote_average"])
+            df["vote_count"] = int(response.json()["vote_count"])
+            df["belongs_to_collection"] = response.json()["belongs_to_collection"]
 
+            url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=279ec8b5e677bfd655c30c6403e14469"
 
-            url = f'https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=279ec8b5e677bfd655c30c6403e14469'
-
-                    # get the response
+            # get the response
             response = requests.get(url)
             director_names = []
             producer_names = []
             director_number = 0
             producer_number = 0
             actor_number = 0
-            for i in range(len(response.json()['cast'])):
-                if response.json()['cast'][i]['known_for_department'].lower() == 'acting':
-                    actor_number+=1
-            for i in range(len(response.json()['crew'])):
-                if response.json()['crew'][i]['job'].lower() == 'director':
-                    director_number+=1
-                    director_names.append(response.json()['crew'][i]['name'])
-                if response.json()['crew'][i]['job'].lower() == 'producer':
-                    producer_number+=1
-                    producer_names.append(response.json()['crew'][i]['name'])
-            df['director_name'] = director_names[0]
-            df['producer_name'] = producer_names[0]
-            df['director_number'] = director_number
-            df['producer_number'] = producer_number
-            df['actor_number'] = actor_number
-            features = pd.DataFrame(df, index=['Value'])
+            for i in range(len(response.json()["cast"])):
+                if response.json()["cast"][i]["known_for_department"].lower() == "acting":
+                    actor_number += 1
+            for i in range(len(response.json()["crew"])):
+                if response.json()["crew"][i]["job"].lower() == "director":
+                    director_number += 1
+                    director_names.append(response.json()["crew"][i]["name"])
+                if response.json()["crew"][i]["job"].lower() == "producer":
+                    producer_number += 1
+                    producer_names.append(response.json()["crew"][i]["name"])
+            df["director_name"] = director_names[0]
+            df["producer_name"] = producer_names[0]
+            df["director_number"] = director_number
+            df["producer_number"] = producer_number
+            df["actor_number"] = actor_number
+            features = pd.DataFrame(df, index=["Value"])
 
             return features
         except:
-            st.write('Please write correct movie name')
+            st.write("Please write correct movie name")
             return pd.DataFrame()
 
     col1, col2 = st.columns(2)
     with col1:
         movie_name = col1.text_input("Which movie's revenue do you want to predict?")
     with col2:
-        col2 = st.button('Get Revenue')
+        col2 = st.button("Get Revenue")
         if col2:
             if movie_name:
                 df = user_input_features(movie_name)
                 if not df.empty:
-                    st.header('Specified Input parameters')
+                    st.header("Specified Input parameters")
                     st.write(df)
-                    st.write('---')
+                    st.write("---")
 
-                    my_pipeline = pickle.load(open(MODEL_PATH,"rb"))
+                    my_pipeline = pickle.load(open(MODEL_PATH, "rb"))
                     prediction = my_pipeline.predict(df)
-                    st.header('Prediction of Revenue')
+                    st.header("Prediction of Revenue")
                     prediction = np.expm1(prediction)
                     st.write(prediction[0])
